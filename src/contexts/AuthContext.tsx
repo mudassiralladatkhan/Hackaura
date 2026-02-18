0ef4a46be443f1bc2c21c88f10e20a16f9e68cb7
mudassiralladatkhan <mudassiralladatkhan@gmail.com>
2026-02-18 15:54:53 +0530

    fix: update auth context with netlify identity url

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -22,7 +22,9 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        netlifyIdentity.init();
+        netlifyIdentity.init({
+            APIUrl: 'https://hackaura2026.netlify.app/.netlify/identity'
+        });
 
         const user = netlifyIdentity.currentUser();
         setUser(user);
